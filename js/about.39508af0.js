a459a2ba9d6338a2e77fe8dbf6b739779ddde60f
Argyl P. Rebanal <asukal.mapaet@gmail.com>
2019-04-09 21:45:20 +0800

    Delete about.39508af0.js

--- js/about.39508af0.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var u=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"})},o=[],s=n("2877"),a={},l=Object(s["a"])(a,u,o,!1,null,null,null);l.options.__file="About.vue";e["default"]=l.exports}}]);
-//# sourceMappingURL=about.39508af0.js.map
